8dca4143f0fa0e6c989bed16ccd99f7b65dc8dc9
toastyteejay <lolcakes31@live.com>
2016-11-12 19:52:18 -0600

    angular works now :D

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,17 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var ionicApp = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+ionicApp.controller('MyController', ['$scope', function($scope){
+  $scope.toasts = [{image: "http://the-toast.net/wp-content/uploads/2016/03/toast-1077984_1920.jpg"},
+                  {image: "https://www.ahealthiermichigan.org/wp-content/uploads/2014/09/Transform-toast-into-breakfast.jpg"},
+                {image: "http://www.jpband.com/wp-content/uploads/2009/10/toast.jpg"},
+                {image: "http://chicago-toast.com/img/big-toast-img.png"},
+                {image: "https://hamfistedtales.files.wordpress.com/2016/07/toast.jpg"}]
+}])
+
+ionicApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
